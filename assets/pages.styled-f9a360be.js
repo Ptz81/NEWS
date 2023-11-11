49d316eb6fb3fcc68200818f62bf29b8ecf9def5
Taras Papka <taras.papka@protonmail.com>
2023-11-12 00:06:32 +0200

    Updates

---
+++ assets/pages.styled-f9a360be.js
@@ -0,0 +1,27 @@
+import{s as e}from"./index-f4eaf86f.js";const n=e.div`
+ min-height: calc(100vh - 50px);
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     background-size: cover;
+`,i=e.h1`
+ font-weight: 500;
+     font-size: 48px;
+     text-align: center;
+`,s=e.h2`
+ font-weight: 500;
+     font-size: 24px;
+     text-align: center;
+`,a=e.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: calc(100vh - 50px);
+`,c=e.div`
+  display: flex;
+  justify-content: space-between;
+
+  @media screen and (max-width: 560px) {
+    flex-direction: column;
+  }
+`;export{n as C,a as N,i as T,s as a,c as b};
